341ced59f22be0026670d2fc71bc890356220a6e
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-09-09 14:19:35 -0300

    Checking the connection of the mercado pago with showing payment methods.

--- webhook/views.py
+++ webhook/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import os
 
+import mercadopago
+from django.http import HttpResponse, JsonResponse
+
+sdk = mercadopago.SDK(os.environ.get('ACCESS_TOKEN'))
 
 # Create your views here.
+
+
 def home(request):
     return HttpResponse("Hello, Django!")
+
+
+def methodsPayments(request):
+    payment_methods_response = sdk.payment_methods().list_all()
+    payment_methods = payment_methods_response["response"]
+    return JsonResponse(payment_methods, safe=False)
